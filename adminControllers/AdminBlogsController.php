9d51d8b4e446c9198ed2b6d1331df0ec74afb511
Jodey <594184@edu.rocmn.nl>
2026-06-17 13:57:13 +0200

    kleine dingen voor admin

--- adminControllers/AdminBlogsController.php
+++ adminControllers/AdminBlogsController.php
@@ -2,16 +2,45 @@
 
 namespace adminControllers;
 
+use adminServices\AdminBlogsService;
+
 class AdminBlogsController
 {
+    private AdminBlogsService $blogService;
+
     public function __construct($router)
     {
-
+        $this->blogService = new AdminBlogsService();
 
         $router->get('/admin/blog', [$this, 'blogPage']);
-
+        $router->post('/admin/blog/toevoegen', [$this, 'addBlog']);
     }
-    public function blogPage(){
+
+    public function blogPage(): void
+    {
+        $blogs = $this->blogService->getAllBlogs();
+        $errors = [];
+        $old = [];
+        $successMessage = isset($_GET['created']) ? 'Blog is toegevoegd.' : '';
+
+        require_once __DIR__ . '/../admin/adminBlog.php';
+        die();
+    }
+
+    public function addBlog(): void
+    {
+        $result = $this->blogService->createBlog($_POST);
+
+        if ($result['success']) {
+            header('Location: /admin/blog?created=1');
+            die();
+        }
+
+        $blogs = $this->blogService->getAllBlogs();
+        $errors = $result['errors'];
+        $old = $result['data'];
+        $successMessage = '';
+
         require_once __DIR__ . '/../admin/adminBlog.php';
         die();
     }
--- adminRepositories/AdminBlogsRepository.php
+++ adminRepositories/AdminBlogsRepository.php
@@ -2,7 +2,44 @@
 
 namespace adminRepositories;
 
+use controllers\DatabaseController;
+
 class AdminBlogsRepository
 {
+    private DatabaseController $db;
 
+    public function __construct()
+    {
+        $this->db = new DatabaseController();
+    }
+
+    public function getAllBlogs(): array
+    {
+        return $this->db->read(
+            'SELECT blog_id, title, article, arthor, tag, date, image, excerpt
+             FROM blog
+             ORDER BY date DESC, blog_id DESC'
+        );
+    }
+
+    public function createBlog(array $data): bool
+    {
+        $nextIdRows = $this->db->read('SELECT COALESCE(MAX(blog_id), 0) + 1 AS next_id FROM blog');
+        $nextId = (int) ($nextIdRows[0]['next_id'] ?? 1);
+
+        return $this->db->save(
+            'INSERT INTO blog (blog_id, title, article, arthor, tag, date, image, excerpt)
+             VALUES (:blog_id, :title, :article, :arthor, :tag, :date, :image, :excerpt)',
+            [
+                'blog_id' => $nextId,
+                'title' => $data['title'],
+                'article' => $data['article'],
+                'arthor' => $data['arthor'],
+                'tag' => $data['tag'],
+                'date' => $data['date'],
+                'image' => $data['image'] ?: null,
+                'excerpt' => $data['excerpt'] ?: null,
+            ]
+        );
+    }
 }
