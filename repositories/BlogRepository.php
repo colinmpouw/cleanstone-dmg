7df85b42d3c1032650e23a38e34b81738b1a18df
Jodey <594184@edu.rocmn.nl>
2026-06-17 12:14:29 +0200

    adminblog weer toegevoegd

--- repositories/BlogRepository.php
+++ repositories/BlogRepository.php
@@ -18,6 +18,19 @@ class BlogRepository
         return $this->db->read('SELECT blog_id, title, article, arthor, tag, image, excerpt, date FROM blog ORDER BY date DESC');
     }
 
+    public function createPost(array $data): bool
+    {
+        return $this->db->save(
+            'INSERT INTO blog (title, article, arthor, tag, date) VALUES (:title, :article, :arthor, :tag, NOW())',
+            [
+                'title' => $data['title'],
+                'article' => $data['article'],
+                'arthor' => $data['arthor'],
+                'tag' => $data['tag'],
+            ]
+        );
+    }
+
     public function getPostById(int $id): ?array
     {
         $rows = $this->db->read(
