8cdeb8ea756fa001527a01bf5c1c3a9d1caa77c5
Yoseph <yusupjanadil@gmail.com>
2026-06-23 11:21:48 +0200

    Fix namespace import and update database controller instantiation in AdminBlogsRepository

--- adminRepositories/AdminBlogsRepository.php
+++ adminRepositories/AdminBlogsRepository.php
@@ -10,7 +10,7 @@ class AdminBlogsRepository
 
     public function __construct()
     {
-        $this->db = new AatabaseController();
+        $this->db = new DatabaseController();
     }
 
     public function getAllBlogs(): array
